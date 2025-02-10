f294851d3beeac3b6573f65c4f183b3cb7bc8f6f
Nahman-sigma <yannu.bruh@gmail.com>
2025-02-10 18:55:05 +0000

    Create JobHandler.lua

---
+++ JobHandler.lua
@@ -0,0 +1,29 @@
+local Sell = workspace.Sell.Sell
+local Prompt = workspace.Boxes.Prompt
+local playerService = game:GetService("Players")
+
+
+Prompt.ProximityPrompt.TriggerEnded:Connect(function(player)
+	local BoxTool = game:GetService("ServerStorage").Box:Clone()
+
+	if not player.Character:FindFirstChild("Box") and not player.Backpack:FindFirstChild("Box") then
+		BoxTool.Parent = player.Character
+		Prompt.Sound:Play()
+		player.PlayerGui.SellTag.Enabled = true
+	end
+end)
+
+
+Sell.Touched:Connect(function(hit)
+	local payment = math.random(30, 50)
+	local char = hit.Parent
+	local player = playerService:GetPlayerFromCharacter(char)
+	
+	if char:FindFirstChild("Box") then
+		Sell.Sound1:Play()
+		player.PlayerGui.SellTag.Enabled = false
+		char:FindFirstChild("Box"):Destroy()
+		Sell.Sound2:Play()
+		player.leaderstats.Bux.Value += payment
+	end
+end)
